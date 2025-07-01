f2b2ae4a1b8fa95757e12beade1a971993728d43
Varun-3131 <srivastavvarun48@gmail.com>
2025-07-01 09:47:18 +0530

    FrontEnd

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import {connectDb} from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
+
 dotenv.config();
 
 
@@ -13,6 +15,13 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+
+
+app.use(cors({
+    origin:"http://localhost:5173/",
+    credentials:true
+}));
+
 
 app.listen(PORT, () => {
     console.log('Server started on port-'+ PORT);
