f834611e33efdea35cddebad51e175c98ec21342
Varun-3131 <srivastavvarun48@gmail.com>
2025-07-08 12:03:15 +0530

    Update In Files

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,10 +7,14 @@ import cors from "cors";
 import {app,server} from "./lib/socket.js";
 import {connectDB} from "./lib/db.js";
 
+import path from "path";
+
+
 dotenv.config();
 
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({  limit: "10mb" }));
@@ -27,6 +31,14 @@ app.use("/api/auth",authRoutes)
 
 
 app.use("/api/messages",messageRoutes)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    })
+}
 
 server.listen(PORT, () => {
     console.log('Server started on port-'+ PORT);
